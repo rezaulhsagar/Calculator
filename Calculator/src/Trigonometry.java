c5fdf3be33321c5da39da98e464849964eeb9f93
rezaulhsagar <rezaulhsagar@gmail.com>
2017-10-11 23:31:18 +0600

    Trigonometry class added

---
+++ Calculator/src/Trigonometry.java
@@ -0,0 +1,12 @@
+
+public class Trigonometry {
+	public static double sin(double theta){
+		return Math.sin(theta);
+	}
+	public static double cos(double theta){
+		return Math.cos(theta);
+	}
+	public static double tan(double theta){
+		return Math.tan(theta);
+	}
+}
